1f4e0b384292da081144a7e58a01338d1e1818f5
prakash14india <prakash14india@gmail.com>
2016-09-22 12:02:24 +0530

    Client side javascript

--- server.js
+++ server.js
@@ -2,76 +2,119 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var articles = {
-    a1: {
-        title: "Article One - Prakash",
-        heading: "Article One",
-        date: "Sep 18, 2016",
-        content: `
-            <p>My Sample content appear here, My Sample content appear here, My Sample content appear here, My Sample content appear here.</p>
-            <p>My Sample content appear here, My Sample content appear here, My Sample content appear here, My Sample content appear here.</p>
-            <p>My Sample content appear here, My Sample content appear here, My Sample content appear here, My Sample content appear here.</p>
-            <p>My Sample content appear here, My Sample content appear here, My Sample content appear here, My Sample content appear here.</p>`
-    },
-    a2: {
-        title: "Article Two - Prakash",
-        heading: "Article Two",
-        date: "Sep 17, 2016",
-        content: "<p>My Sample content appear here two article</p>"
-    },
-    a3: {
-        title: "Article Three - Prakash",
-        heading: "Article Three",
-        date: "Sep 15, 2016",
-        content: "<p>My Sample content appear here third article</p>"
-    }
-};
-
-function createTemplate(data) {
-    var htmlTemplate = `
-        <html>
-            <head>
-                <title>${data.title}</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link href="/ui/style.css" rel="stylesheet" />
-            </head>
-            <body>
-                <div class="container">
-                    <div>
-                        <a href="/">Home</a>
-                        <a href="/a1">Article One</a>
-                        <a href="/a2">Article Two</a>
-                        <a href="/a3">Article Three</a>
-                    </div>
-                    <hr/>
-                    <h3>${data.heading}</h3>
-                    <div>
-                        ${data.date}
-                    </div>
-                    <div>
-                        ${data.content}
-                    </div>
-                </div>
-            </body>
-        </html>    
-    `
-    return htmlTemplate;
-}
-
-
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+var articles = {
+        'article-one' : {
+        title : "Article one | Shrayan Banerjee",
+        heading : "Article one",
+        date : "Sep 5 , 2016",
+        content: `<p><font color="687584">
+        <b>DeepMind</b> Technologies Limited is a British artificial intelligence company founded in September 2010. It was acquired by Google in 2014. The company has created a neural network that learns how to play video games in a fashion similar to that of humans, as well as a Neural Turing Machine, or a neural network that may be able to access an external memory like a conventional Turing machine, resulting in a computer that mimics the short-term memory of the human brain.
+        
+        The company made headlines in 2016 after its AlphaGo program beat a human professional<b> Go </b> player for the first time.
+        </font></p>
+        <p><font color="688484">
+        <b>DeepMind</b> Technologies Limited is a British artificial intelligence company founded in September 2010. It was acquired by Google in 2014. The company has created a neural network that learns how to play video games in a fashion similar to that of humans, as well as a Neural Turing Machine, or a neural network that may be able to access an external memory like a conventional Turing machine, resulting in a computer that mimics the short-term memory of the human brain.
+        
+        The company made headlines in 2016 after its AlphaGo program beat a human professional<b> Go </b> player for the first time.
+        </font></p>
+        
+        <p><font color="681184">
+        <b>DeepMind</b> Technologies Limited is a British artificial intelligence company founded in September 2010. It was acquired by Google in 2014. The company has created a neural network that learns how to play video games in a fashion similar to that of humans, as well as a Neural Turing Machine, or a neural network that may be able to access an external memory like a conventional Turing machine, resulting in a computer that mimics the short-term memory of the human brain.
+        
+        The company made headlines in 2016 after its AlphaGo program beat a human professional<b> Go </b> player for the first time.
+        </font></p>`
+},
+        'article-two' : {
+            title : "Article two | Shrayan Banerjee",
+        heading : "Article two",
+        date : "Sep 10 , 2016",
+        content: `<p><font color="687584">
+        <b>DeepMind</b> Technologies Limited is a British artificial intelligence company founded in September 2010. It was acquired by Google in 2014. The company has created a neural network that learns how to play video games in a fashion similar to that of humans, as well as a Neural Turing Machine, or a neural network that may be able to access an external memory like a conventional Turing machine, resulting in a computer that mimics the short-term memory of the human brain.
+        
+        The company made headlines in 2016 after its AlphaGo program beat a human professional<b> Go </b> player for the first time.
+        </font></p>
+        <p><font color="688484">
+        <b>DeepMind</b> Technologies Limited is a British artificial intelligence company founded in September 2010. It was acquired by Google in 2014. The company has created a neural network that learns how to play video games in a fashion similar to that of humans, as well as a Neural Turing Machine, or a neural network that may be able to access an external memory like a conventional Turing machine, resulting in a computer that mimics the short-term memory of the human brain.
+        
+        The company made headlines in 2016 after its AlphaGo program beat a human professional<b> Go </b> player for the first time.
+        </font></p>
+        
+        <p><font color="681184">
+        <b>DeepMind</b> Technologies Limited is a British artificial intelligence company founded in September 2010. It was acquired by Google in 2014. The company has created a neural network that learns how to play video games in a fashion similar to that of humans, as well as a Neural Turing Machine, or a neural network that may be able to access an external memory like a conventional Turing machine, resulting in a computer that mimics the short-term memory of the human brain.
+        
+        The company made headlines in 2016 after its AlphaGo program beat a human professional<b> Go </b> player for the first time.
+        </font></p>`
+            
+        },
+        'article-three' : {
+            title : "Article three | Shrayan Banerjee",
+        heading : "Article three",
+        date : "Sep 15 , 2016",
+        content: `<p><font color="687584">
+        <b>DeepMind</b> Technologies Limited is a British artificial intelligence company founded in September 2010. It was acquired by Google in 2014. The company has created a neural network that learns how to play video games in a fashion similar to that of humans, as well as a Neural Turing Machine, or a neural network that may be able to access an external memory like a conventional Turing machine, resulting in a computer that mimics the short-term memory of the human brain.
+        
+        The company made headlines in 2016 after its AlphaGo program beat a human professional<b> Go </b> player for the first time.
+        </font></p>
+        <p><font color="688484">
+        <b>DeepMind</b> Technologies Limited is a British artificial intelligence company founded in September 2010. It was acquired by Google in 2014. The company has created a neural network that learns how to play video games in a fashion similar to that of humans, as well as a Neural Turing Machine, or a neural network that may be able to access an external memory like a conventional Turing machine, resulting in a computer that mimics the short-term memory of the human brain.
+        
+        The company made headlines in 2016 after its AlphaGo program beat a human professional<b> Go </b> player for the first time.
+        </font></p>
+        
+        <p><font color="681184">
+        <b>DeepMind</b> Technologies Limited is a British artificial intelligence company founded in September 2010. It was acquired by Google in 2014. The company has created a neural network that learns how to play video games in a fashion similar to that of humans, as well as a Neural Turing Machine, or a neural network that may be able to access an external memory like a conventional Turing machine, resulting in a computer that mimics the short-term memory of the human brain.
+        
+        The company made headlines in 2016 after its AlphaGo program beat a human professional<b> Go </b> player for the first time.
+        </font></p>`
+        },
+};
+        
+function createtemplate (data) 
+{
+    var title= data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmltemplate=`
+<!doctype html>
+<html>
+<body>
+<link href="/ui/style.css" rel="stylesheet" />
+
+<div class="container">
+    <div>
+        <a href="/">Home</a>
+    </div>
+    <hr/>
+    <h1>
+        This is my first article about deep mind 
+    </h1>
+    <div>
+	${heading};
+    </div>
+    <div>
+    ${date}
+    </div>
+        <div>
+    ${content}
+</div>
+</body>
+</html>
+`;
+return htmltemplate;
+}
+app.get('/',function(req,res){
+res.sendFile(path.join(__dirname,'ui','index.html'));
 });
+
+
 app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+  var articleName= req.params.articleName;
+  res.send(createtemplate(articles[articleName]));
 });
-
-
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -81,6 +124,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,29 @@
 console.log('Loaded!');
+
+//Get Name Content change value
+var contentId = document.getElementById("Name");
+contentId.innerHTML = "&#x2013; Prakash Venkatesan";
+
+var imgMadi = document.getElementById("imgMadi");
+var marginSize = 0;
+var marginMoveLeft = true;
+
+//Movement Image
+function moveImg() {
+	if (marginSize === 0) {
+		marginMoveLeft =  true;
+	}
+	
+	if (marginSize <=500  && marginMoveLeft === true) {
+		marginSize = marginSize + 10;
+		imgMadi.style.marginLeft = marginSize + 'px';
+	} else {
+		marginMoveLeft = false;
+		marginSize = marginSize - 10;
+		imgMadi.style.marginLeft = marginSize + 'px';
+	}
+}
+
+imgMadi.onclick = function() {
+	var interval = setInterval(moveImg, 50);
+}

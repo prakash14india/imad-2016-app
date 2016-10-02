2d6a3b1ee49d5b93fc966d4288165b0837af3d7c
prakash14india <prakash14india@gmail.com>
2016-10-02 19:44:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -55,14 +55,28 @@ var name = document.getElementById("name");
 var name = name.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
-    //Make request get server name
-
-    //render the value in List
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = "";
-    for (var i=0;i<names.length;i++) {
-        list += '<li>' + names[i] + "</li>"; 
+     //Create request to the counter endpoint
+    var req = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    req.onreadystatechange = function() {
+        if (req.readyState === XMLHttpRequest.DONE) {
+            //Take same action
+            if (req.status === 200) {
+                //render the value in List
+                var names = req.responseText;
+                names = JSON.parse(names);
+                var list = "";
+                for (var i=0;i<names.length;i++) {
+                    list += '<li>' + names[i] + "</li>"; 
+                }
+                var ul = document.getElementById('nameList');
+                ul.innerHTML = list;
+            }
+        }
     }
-    var ul = document.getElementById('nameList');
-    ul.innerHTML = list;
+    
+    //Make the request
+    req.open('GET', 'http://prakash14india.imad.hasura-app.io/submit-name?name='+ name, true);
+    req.send(null);
 }

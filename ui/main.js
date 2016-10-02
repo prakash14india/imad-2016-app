155bca71e90094c4b6e2ab56b8909f5dbe908b8f
prakash14india <prakash14india@gmail.com>
2016-10-02 19:04:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,3 +27,17 @@ function moveImg() {
 imgMadi.onclick = function() {
 	var interval = setInterval(moveImg, 50);
 }
+//Count code
+var btnCounter = document.getElementById("counter");
+var counter = 0;
+
+btnCounter.onclick = function() {
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+}
